376e5943601dc7dafe1722d5daf8853efe951063
Hector Prieto <hprieto@endava.com>
2018-02-02 17:02:18 -0500

    latest changes endava

--- server.js
+++ server.js
@@ -7,12 +7,16 @@ const indexHTML = (() => {
   return fs.readFileSync(path.resolve(__dirname, "./index.html"), "utf-8");
 })();
 
+app.use("/dist", express.static(path.resolve(__dirname, "./dist")));
+
+require("./build/dev-server")(app);
+
 app.get("*", (req, res) => {
   res.write(indexHTML);
   res.end();
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`server started at http://localhost:${port}`);
 });
