1479c119e33f6a9417199c05f4d084023b6a0762
fedelogos <fedelogos@hotmail.com>
2018-02-04 21:24:21 -0500

    applicaion files

--- build/dev-server.js
+++ build/dev-server.js
@@ -1,7 +1,15 @@
-const webpack = require("webpack");
-const clientConfig = require("./webpack.client.config");
+const webpack = require('webpack')
+const clientConfig = require('./webpack.client.config')
 
-module.exports = function setupDevServer(app){
+module.exports = function setupDevServer (app) {
+  clientConfig.entry.app = [
+    'webpack-hot-middleware/client',
+    clientConfig.entry.app
+  ]
+  clientConfig.plugins.push(
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  )
   const clientCompiler = webpack(clientConfig)
   app.use(
     require('webpack-dev-middleware')(clientCompiler, {
@@ -9,6 +17,6 @@ module.exports = function setupDevServer(app){
         colors: true
       }
     })
-  );
+  )
   app.use(require('webpack-hot-middleware')(clientCompiler))
-};
+}
--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
-import Vue from "vue";
+import Vue from 'vue'
 
 const app = new Vue({
   data: {
-    hello: "hi there"
-  }
+    hello: 'hi there5'
+  },
+  template: '<div id="app">{{ hello }}</div>'
 })
 
-export {app};
+export { app }
